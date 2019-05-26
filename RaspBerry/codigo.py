09236ae12a9a1ad1d9b3d6997e5ba96c9c16e5c3
Josue Valenzuela <val171001@uvg.edu.gt>
2019-05-26 16:41:37 -0600

    add post request to python script

--- RaspBerry/codigo.py
+++ RaspBerry/codigo.py
@@ -11,9 +11,23 @@
 #__importamos la libreria tkinter
 #__interfaz Grafica
 from tkinter import *
+from uuid import uuid4 as uuid
+import pyqrcode
+from collections import OrderedDict
+import requests
 
 #__variable de ejemplo para los codigos
-ejemplo="4dgasff5asefdd5e5"
+ejemplo=uuid().hex[:15]
+
+code = pyqrcode.create(ejemplo)
+
+url = 'https://hci-server-uvg.herokuapp.com/machine/code'
+params = OrderedDict([('code', ejemplo), ('points', 10)])
+
+results = requests.post(url, data={'code': ejemplo, 'points':10})
+print(results)
+
+code.png('code.png', scale=8)
 
 
 
@@ -53,6 +67,8 @@ nombre =Label(frame,text="Tu codigo es:",fg="black", font=("Comic Sans MS",12)).
 #__titulo
 codigo =Label(frame,text=ejemplo,fg="forestgreen", font=("Comic Sans MS",50)).place(x=30,y=100)
 
+imagen_qr=PhotoImage(file="code.png")
+lblcode=Label (raiz,image=imagen_qr).place(x=0,y=0)
 
 #__informacion_parrafo
 imformacion_parrafo=Label(frame,text="Gracias por ayudar a salvar nuestro planeta esperamos que regreses pronto.",fg="black", font=("Comic Sans MS",12),wraplength=700,anchor="center").place(x=25,y=230)
